edcb6058a9fe8548978cea7c2bf1faadc7d2a9b0
gintscelmins-star <gints.celmins@gmail.com>
2026-04-10 10:41:10 +0300

    fix: pievienots CSP header GA4 atbalstam

--- next.config.js
+++ next.config.js
@@ -13,6 +13,26 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+              "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net",
+              "img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://res.cloudinary.com",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' https://fonts.gstatic.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
